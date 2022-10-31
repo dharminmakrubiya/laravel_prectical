1a0c588bed4aa075fb7c1182afa11fd32d801b42
dharmin <dharminmakrubiya@hotmail.com>
2022-10-31 18:54:23 +0530

    issue fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,36 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('You are logged in!') }}
                 </div>
 
-                <div class="card-body">
-                    <form action="/upload" method="post" enctype="multipart/form-data">
-                        @csrf
-                        <label for="formFileLg" class="form-label">Please Upload Your Profile Picture</label><br>
-                        <input type="file" name="image" />
-                        <input type="submit" value="Upload" />
-                    </form>
+                <div class="container">
+                    <div class="card-body">
+                    @if(session()->has('message'))
+                    <div class="alert alert-success">{{session()->get('message')}}</div>
+
+                    @elseif(session()->has('error'))
+                    <div class="alert alert-danger">{{session()->get('error')}}</div>
+                    @endif
+
+                        <form action="/upload" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <label class="form-label">Please Upload Your Profile Picture</label><br>
+                            <input type="file" name="image" />
+                            <input type="submit" value="Upload" />
+                        </form>
+                    </div>
                 </div>
 
             </div>
--- routes/web.php
+++ routes/web.php
@@ -36,17 +36,23 @@ Route::get('/send-email', [EmailController::class, 'index']);
 Route::post('/upload',function(Request $request){
     // $request->image->store('images','public');
 
+    $request->validate([
+        'image.*' => 'mimes:jpeg,jpg,png,gif,csv,txt,pdf|max:2048'
+    ]);
+
     if ($request->hasFile('image')) {
         $filename = $request->image->getClientOriginalName();
+        $request->session()->flash('message','Your Profile Picture was Uploaded Successfully.');
         
         if (auth()->user()->avatar) {
             Storage::delete('/public/images/'.auth()->user()->avatar);
         }
-
+        
+        // $this->validate($request,['avatar'=> 'required|avatar|mimes:jpg,png,jpeg,gif,svg|max:2048']);
         $request->image->storeAs('images',$filename,'public');
         auth()->User()->update(['avatar' => $filename]);
     }
-
-    return 'Your Profile Picture Uploaded Successfully.';
+    $request->session()->flash('error','Please choose your profile picture!');
+    return redirect()->back();
     // dd(request()->file('image'));
 });
